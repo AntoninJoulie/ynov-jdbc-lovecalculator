0e0ed987256c4f33abe9ebafc7c5720569c1dcfa
Pascal Lemoine <pascal.lemoine@gmail.com>
2018-11-08 12:20:56 +0100

    implémentations

--- src/main/java/com/example/demo/controllers/LoveCalculatorController.java
+++ src/main/java/com/example/demo/controllers/LoveCalculatorController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controllers;
 
 import com.example.demo.api.LoveResult;
+import com.example.demo.dto.LoveHistory;
 import com.example.demo.services.LoveCalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class LoveCalculatorController {
@@ -13,7 +18,12 @@ public class LoveCalculatorController {
     @Autowired
     private LoveCalculatorService loveCalculatorService;
 
-    @GetMapping("test-api")
+    @GetMapping("ping")
+    public String ping() {
+        return "pong";
+    }
+
+    @GetMapping("test")
     public LoveResult testApi(
             @RequestParam String firstname,
             @RequestParam String secondname
@@ -21,6 +31,22 @@ public class LoveCalculatorController {
         return loveCalculatorService.testApi(firstname, secondname);
     }
 
+    @GetMapping("names")
+    public List<String> getAllNames() throws SQLException {
+        return loveCalculatorService.getAllNames();
+    }
 
+    @GetMapping("history")
+    public List<LoveHistory> getHistory(@RequestParam Optional<String> name) throws SQLException {
+        return loveCalculatorService.getHistory(name);
+    }
+
+    @GetMapping("love")
+    public LoveHistory love(
+            @RequestParam String firstname,
+            @RequestParam String secondname
+    ) throws SQLException {
+        return loveCalculatorService.love(firstname, secondname);
+    }
 
 }
--- src/main/java/com/example/demo/services/LoveCalculatorService.java
+++ src/main/java/com/example/demo/services/LoveCalculatorService.java
@@ -2,17 +2,116 @@ package com.example.demo.services;
 
 import com.example.demo.api.LoveCalculatorApi;
 import com.example.demo.api.LoveResult;
+import com.example.demo.configuration.DbConfiguration;
+import com.example.demo.dto.LoveHistory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class LoveCalculatorService {
 
     @Autowired
+    private DbConfiguration dbConfiguration;
+
+    @Autowired
     private LoveCalculatorApi loveCalculatorApi;
+
+    private Connection connection;
 
     public LoveResult testApi(String firstName, String secondName) {
         return this.loveCalculatorApi.bringMeLove(firstName, secondName);
+    }
+
+    /**
+     * Cours #1
+     *
+     * Initialiser la connexion à la base de données
+     * grace aux paramètres de configuration {@link DbConfiguration}
+     *
+     */
+    @PostConstruct
+    public void init() throws SQLException {
+    }
+
+    /**
+     * Cours #2
+     *
+     * Retourner la liste de tous les noms de la base de données.
+     *
+     * Test
+     * - http://localhost:8080/names
+     *
+     */
+    public List<String> getAllNames() throws SQLException {
+        return null;
+    }
+
+    /**
+     * TD #1
+     *
+     * A partir de l'exemple ci-dessus, retourner une liste de @{@link LoveHistory}
+     * représentant tout l'historique contenant pour chaque ligne les noms des deux personnes
+     * et leur résultat.
+     *
+     * Aide
+     * - Des jointures sql seront nécessaires pour lier les tables entre-elles.
+     *
+     * Test
+     * - http://localhost:8080/history
+     *
+     * =======
+     *
+     * TD #2
+     *
+     * Améliorer votre code pour retourner tout l'historique si le paramètre 'name'
+     * n'est pas présent, et l'historique filtré pour une seule personne si le paramètre 'name'
+     * est présent.
+     *
+     * Aide
+     * - Rechercher sur internet comment utiliser @{@link PreparedStatement}.
+     * - Vérifier la présence du paramètre 'name' par 'name.isPresent()'
+     * - Récupérer la valeur de 'name' par 'name.get()'
+     * - une clause sql where sera nécessaire pour filtrer les noms.
+     *
+     * Test
+     * - http://localhost:8080/history?name=links
+     *
+     */
+    public List<LoveHistory> getHistory(Optional<String> name) throws SQLException {
+        return null;
+    }
+
+    /**
+     * TD #3
+     *
+     * En utilisant les deux paramètres 'firstname' et 'secondname' :
+     * - appeler le {@link LoveCalculatorApi} pour calculer le résultat
+     * - puis insérer le résultat dans l'historique de la base de données
+     * - enfin retourner un {@link LoveHistory} contenant les deux noms et le résultat
+     *
+     * /!\ Attention !
+     * Le nom est défini comme étant unique dans la table 'names'.
+     * Si le nom n'existe pas en base, il devra etre insérer avant l'insertion de l'historique
+     * pour lui attribuer un id.
+     *
+     * Aide
+     * - Rechercher sur internet comment récupérer le nombre de ligne d'un {@link ResultSet}
+     * - Si le nombre de ligne est = 0, la requete n'a trouvé aucun résultat.
+     * - Rechercher sur internet comment récupérer l'id d'une ligne créée : Statement.RETURN_GENERATED_KEYS
+     * - Factoriser votre code au maximum afin d'optimiser votre algorithme.
+     *
+     * Test
+     * - http://localhost:8080/love?firstname=mario&secondname=peach
+     *
+     */
+    public LoveHistory love(String firstname, String secondname) throws SQLException {
+        return null;
     }
 
 }
